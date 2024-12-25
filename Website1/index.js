3749ea655d71f70a571cf20de9cb7622cf3f7ba7
Jzy <jestalycastillo15@gmail.com>
2024-12-25 23:14:31 +0800

    worked with js

--- Website1/index.js
+++ Website1/index.js
@@ -1,2 +1,36 @@
-console.log("Hello World!");
-console.log("Hello World number 2");
+// console.log("Hello World!");
+// console.log("Hello World number 2");
+// console.log("Hello World number 3");
+
+// let x = 100;
+// const y = 5;
+
+// console.log(x);
+//console.log(y);
+
+// let myName = "Jestaly";
+// let age = 20;
+// let salary = 75095.34;
+
+// console.log("Name: " + myName);
+// console.log("Age: " + age);
+// console.log("Salary: " + salary);
+
+//Use template to allow variable insertion
+// const x = 5;
+// const y = 0;
+// let result = x % y;
+// console.log(typeof y)
+// console.log(`the number ${result} is the result`)
+// console.log("x: " + x);
+// console.log("y: " + y);
+// console.log(result)
+
+let myName = window.prompt("type your name");
+
+console.log(`Welcome ${myName }`);
+
+
+
+
+
