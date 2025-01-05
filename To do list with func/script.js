b0a34f5b47b615ad26e07569956336feb6939bad
Jzy <jestalycastillo15@gmail.com>
2025-01-05 18:46:14 +0800

    added function for completed tasks

--- To do list with func/script.js
+++ To do list with func/script.js
@@ -1,12 +1,15 @@
 
 const mainInput = document.getElementById("main-input");
 const taskContainer = document.getElementById("to-do-container");
-const compContainer = document.getElementById("complete-container");
+const compContainer = document.getElementById("complete-task-container");
+
 let id = 0;
+
 function addTask(){
     if(String(mainInput.value).trim() == "") return;
     
     id++;
+
     let task = document.getElementById("main-input").value;
     taskContainer.innerHTML += `<div class="task-box" id="task-box-${id}"> ${task} <button onclick="addCheck(event)" class="check-box" id="check-box-${id}"></button></div>`;
     mainInput.value = "";
@@ -15,10 +18,15 @@ function addTask(){
 function addCheck(event){
     let checkBox = event.target;
     let taskBox = checkBox.parentElement;
+    let textContent = document.getElementById(taskBox.id).textContent;
     taskBox.style.backgroundColor = 'rgb(130, 129, 129)';
     checkBox.style.backgroundColor = 'rgb(205, 205, 205)';  
-
     setTimeout(() => {
+        addCompletedTask(textContent);
         taskBox.remove();
     }, 300);
+}
+
+function addCompletedTask(textContent){
+    compContainer.innerHTML += `<div class="complete-box">${textContent}</div>`;
 }
