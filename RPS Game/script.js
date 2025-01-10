afc11e35902c0a2a34d13987a9680db9057b0b27
Jzy <jestalycastillo15@gmail.com>
2025-01-10 20:10:14 +0800

    added game indicator

--- RPS Game/script.js
+++ RPS Game/script.js
@@ -18,7 +18,7 @@ const switchHand = () => {
 
 }
 
-const randomizeBot = () => {
+const getBotHand = () => {
     const bot = document.getElementById("bot-container");
 
     const RandomNum = Math.floor((Math.random() * 3)) + 1;
@@ -39,65 +39,67 @@ const randomizeBot = () => {
     recordScore(botSource);
 
 }
+const removeIndicator = () =>{
+    const indicator = document.getElementById("indicator-container");
+    indicator.style.display = 'none';
+}
 
 const recordScore = (botSource) =>{
+    const indicator = document.getElementById("indicator-container");
+    const indicatorLabel = document.getElementById("notification");
     const user = document.getElementById("user-hand");
     const userSource = String(user.src);
 
     if((userSource.match("rock") && botSource.match("rock")) 
         || (userSource.match("paper") && botSource.match("paper")) 
             ||(userSource.match("scissor") && botSource.match("scissor"))){
-        return;
+                indicator.style.backgroundColor = 'hsl(0, 0.00%, 72.90%)';
+                indicatorLabel.textContent = "Draw";
+                return;
     }
     if((userSource.match("rock") && botSource.match("paper")) 
         || (userSource.match("paper") && botSource.match("scissor")) 
             || (userSource.match("scissor") && botSource.match("rock"))){
-        botScore++;
+                indicator.style.backgroundColor = 'hsl(0, 70.50%, 56.10%, 1.00)';
+                indicatorLabel.textContent = "You Lose!";
+                botScore++;
     }
     if((userSource.match("rock") && botSource.match("scissor")) 
         || (userSource.match("paper") && botSource.match("rock"))
             ||(userSource.match("scissor") && botSource.match("paper"))){
-        userScore++;
+                indicator.style.backgroundColor = 'hsl(104, 41%, 65%)';
+                indicatorLabel.textContent = "You Won!";
+                userScore++;
     }
 
     console.log("User: " + userScore);
     console.log("Bot: " + botScore);
 }
 
+
+let count = 0;
+let randomizedHand = "scissor";
+const bot = document.getElementById("bot-container");
 function randomizeBotHand(){
     if(count === 25){
         count = 0;
-        randomizeBot();
+        getBotHand();
         return;
     }
- 
-     setTimeout(() => {
-        if(hand === "rock"){
-            hand = "paper";
-        } else if (hand === "paper"){
-            hand = "scissor";
-        } else {
-            hand = "rock";
-        }
-        bot.innerHTML = `<img id="bot-hand" src="Hands/${hand}_flip.png"/>`
-        count++;
-        randomizeBotHand();
-    }, 100);
+    randomize();
 }
 
-let count = 0;
-let hand = null;
-const bot = document.getElementById("bot-container");
-
-async  function randomize(){
+function randomize(){
     setTimeout(() => {
-        if(hand === "rock"){
-            hand = "paper";
-        } else if (hand === "paper"){
-            hand = "scissor";
+        if(randomizedHand === "rock"){
+            randomizedHand = "paper";
+        } else if (randomizedHand === "paper"){
+            randomizedHand = "scissor";
         } else {
-            hand = "rock";
+            randomizedHand = "rock";
         }
-        bot.innerHTML = `<img id="bot-hand" src="Hands/${hand}_flip.png"/>`;
-    }, 2000);
+        bot.innerHTML = `<img id="bot-hand" src="Hands/${randomizedHand}_flip.png"/>`;
+        count++
+        randomizeBotHand();
+    }, 100);
 }
