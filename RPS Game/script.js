84d25cc3e61d1fd57020932aae509e0f7e4cc114
Jzy <jestalycastillo15@gmail.com>
2025-01-11 15:17:46 +0800

    Enhance code performance

--- RPS Game/script.js
+++ RPS Game/script.js
@@ -1,8 +1,15 @@
-let userScore = document.getElementById("user-score");
-let botScore = document.getElementById("bot-score");
+
+const userScore = document.getElementById("user-score");
+const botScore = document.getElementById("bot-score");
+const user = document.getElementById("user-container");
+const bot = document.getElementById("bot-container");
+const botSelection = document.getElementById("bot-pick-label");
+const indicator = document.getElementById("indicator-container");
+const indicatorLabel = document.getElementById("notification");
+const pickButton = document.getElementById("pick-button");
+const switchButton = document.getElementById("user-pick-button");
 
 const switchHand = () => {
-    const user = document.getElementById("user-container");
     const hand = document.getElementById("user-hand");
     const handSource = String(hand.src);
 
@@ -16,47 +23,51 @@ const switchHand = () => {
         user.innerHTML = `<img id="user-hand" src="Hands/rock.png"/>`
     }
 
-    indicator.style.display = 'none';
+    displayIndicator("none");
+}
 
+const displayIndicator = (display) =>{
+    indicator.style.display = display;
 }
 
 const getBotHand = () => {
     const bot = document.getElementById("bot-container");
+    const randomNum = Math.floor((Math.random() * 3)) + 1;
 
-    const RandomNum = Math.floor((Math.random() * 3)) + 1;
-
-    if(RandomNum === 1){
-        bot.innerHTML = `<img id="bot-hand" src="Hands/rock_flip.png"/>`
-        botSelection.textContent = "Rock";
+    switch(randomNum){
+        case 1:
+            bot.innerHTML = `<img id="bot-hand" src="Hands/rock_flip.png"/>`
+            botSelection.textContent = "Rock";
+            break;
+        case 2:
+            bot.innerHTML = `<img id="bot-hand" src="Hands/scissor_flip.png"/>`
+            botSelection.textContent = "Scissor";
+            break;
+        case 3:
+            bot.innerHTML = `<img id="bot-hand" src="Hands/paper_flip.png"/>`
+            botSelection.textContent = "Paper";
+            break;          
+        default:
+            bot.innerHTML = `<img id="bot-hand" src="Hands/rock_flip.png"/>`
+            botSelection.textContent = "Rock";
+            break;               
     }
-    else if(RandomNum === 2){
-          bot.innerHTML = `<img id="bot-hand" src="Hands/scissor_flip.png"/>`
-          botSelection.textContent = "Scissor";
-        }
-    else if(RandomNum === 3){
-          bot.innerHTML = `<img id="bot-hand" src="Hands/paper_flip.png"/>`
-          botSelection.textContent = "Paper";
-        }
 
     const botHand = document.getElementById("bot-hand");
     const botSource = String(botHand.src);
 
     recordScore(botSource);
-
 }
 
-const removeIndicator = () =>{
-    const indicator = document.getElementById("indicator-container");
-    indicator.style.display = 'none';
-}
+const removeIndicator = () => displayIndicator("none");
+
+const enablePickAndSwitch = (isEnabled) => switchButton.disabled = pickButton.disabled = isEnabled;
 
 const recordScore = (botSource) =>{
-    const indicator = document.getElementById("indicator-container");
-    const indicatorLabel = document.getElementById("notification");
     const user = document.getElementById("user-hand");
     const userSource = String(user.src);
 
-    indicator.style.display = 'flex';
+    displayIndicator("flex");
 
     if((userSource.match("rock") && botSource.match("rock")) 
         || (userSource.match("paper") && botSource.match("paper")) 
@@ -64,14 +75,14 @@ const recordScore = (botSource) =>{
                 indicator.style.backgroundColor = 'hsl(0, 0.00%, 72.90%)';
                 indicatorLabel.textContent = "Draw";
     }
-    if((userSource.match("rock") && botSource.match("paper")) 
+    else if((userSource.match("rock") && botSource.match("paper")) 
         || (userSource.match("paper") && botSource.match("scissor")) 
             || (userSource.match("scissor") && botSource.match("rock"))){
                 indicator.style.backgroundColor = 'hsl(0, 70.50%, 56.10%, 1.00)';
                 indicatorLabel.textContent = "You Lose!";
                 botScore.textContent++;
     }
-    if((userSource.match("rock") && botSource.match("scissor")) 
+    else if((userSource.match("rock") && botSource.match("scissor")) 
         || (userSource.match("paper") && botSource.match("rock"))
             ||(userSource.match("scissor") && botSource.match("paper"))){
                 indicator.style.backgroundColor = 'hsl(104, 41%, 65%)';
@@ -79,17 +90,13 @@ const recordScore = (botSource) =>{
                 userScore.textContent++;
     }
 
-    pickButton.disabled = false;
-    switchButton.disabled = false;
+    enablePickAndSwitch(false);
 }
+
 
 let count = 0;
 let randomizedHand = "scissor";
-const bot = document.getElementById("bot-container");
-const botSelection = document.getElementById("bot-pick-label");
-const indicator = document.getElementById("indicator-container");
-const pickButton = document.getElementById("pick-button");
-const switchButton = document.getElementById("user-pick-button");
+
 function randomizeBotHand(){
     if(count === 25){
         count = 0;
@@ -108,17 +115,23 @@ function randomize(){
             case "Paper":
                 randomizedHand = "Scissor";
                 break;
+            case "Scissor":
+                randomizedHand = "Rock";
+                break;
             default:
                 randomizedHand = "Rock";
                 break;
         }
 
-        pickButton.disabled = true;
-        switchButton.disabled = true;
+        enablePickAndSwitch(true);
+
         botSelection.textContent = randomizedHand;
         bot.innerHTML = `<img id="bot-hand" src="Hands/${randomizedHand.toLowerCase()}_flip.png"/>`;
-        indicator.style.display = 'none';
+        
+        displayIndicator("none");
+        
         count++
+
         randomizeBotHand();
     }, 100);
 }
