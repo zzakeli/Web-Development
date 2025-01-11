ca5a51f9fa7e752204affa2410210668fe79e7e5
Jzy <jestalycastillo15@gmail.com>
2025-01-11 13:02:21 +0800

    updated scoring mechanism

--- RPS Game/script.js
+++ RPS Game/script.js
@@ -1,5 +1,5 @@
-let userScore = 0;
-let botScore = 0;
+let userScore = document.getElementById("user-score");
+let botScore = document.getElementById("bot-score");
 
 const switchHand = () => {
     const user = document.getElementById("user-container");
@@ -15,6 +15,8 @@ const switchHand = () => {
     else if(handSource.match("paper")){
         user.innerHTML = `<img id="user-hand" src="Hands/rock.png"/>`
     }
+
+    indicator.style.display = 'none';
 
 }
 
@@ -61,25 +63,24 @@ const recordScore = (botSource) =>{
             ||(userSource.match("scissor") && botSource.match("scissor"))){
                 indicator.style.backgroundColor = 'hsl(0, 0.00%, 72.90%)';
                 indicatorLabel.textContent = "Draw";
-                return;
     }
     if((userSource.match("rock") && botSource.match("paper")) 
         || (userSource.match("paper") && botSource.match("scissor")) 
             || (userSource.match("scissor") && botSource.match("rock"))){
                 indicator.style.backgroundColor = 'hsl(0, 70.50%, 56.10%, 1.00)';
                 indicatorLabel.textContent = "You Lose!";
-                botScore++;
+                botScore.textContent++;
     }
     if((userSource.match("rock") && botSource.match("scissor")) 
         || (userSource.match("paper") && botSource.match("rock"))
             ||(userSource.match("scissor") && botSource.match("paper"))){
                 indicator.style.backgroundColor = 'hsl(104, 41%, 65%)';
                 indicatorLabel.textContent = "You Won!";
-                userScore++;
+                userScore.textContent++;
     }
 
-    console.log("User: " + userScore);
-    console.log("Bot: " + botScore);
+    pickButton.disabled = false;
+    switchButton.disabled = false;
 }
 
 let count = 0;
@@ -87,6 +88,8 @@ let randomizedHand = "scissor";
 const bot = document.getElementById("bot-container");
 const botSelection = document.getElementById("bot-pick-label");
 const indicator = document.getElementById("indicator-container");
+const pickButton = document.getElementById("pick-button");
+const switchButton = document.getElementById("user-pick-button");
 function randomizeBotHand(){
     if(count === 25){
         count = 0;
@@ -98,15 +101,20 @@ function randomizeBotHand(){
 
 function randomize(){
     setTimeout(() => {
-        if(randomizedHand === "Rock"){
-            randomizedHand = "Paper";
-            
-        } else if (randomizedHand === "Paper"){
-            randomizedHand = "Scissor";
-        } else {
-            randomizedHand = "Rock";
+        switch(randomizedHand){
+            case "Rock":
+                randomizedHand = "Paper";
+                break;
+            case "Paper":
+                randomizedHand = "Scissor";
+                break;
+            default:
+                randomizedHand = "Rock";
+                break;
         }
 
+        pickButton.disabled = true;
+        switchButton.disabled = true;
         botSelection.textContent = randomizedHand;
         bot.innerHTML = `<img id="bot-hand" src="Hands/${randomizedHand.toLowerCase()}_flip.png"/>`;
         indicator.style.display = 'none';
