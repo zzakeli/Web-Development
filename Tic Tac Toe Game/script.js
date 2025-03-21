960e5cce2d3431c77f08741d18bee5a01b320612
Jzy <jestalycastillo15@gmail.com>
2025-03-21 12:38:45 +0800

    added tile generation

--- Tic Tac Toe Game/script.js
+++ Tic Tac Toe Game/script.js
@@ -1,10 +1,35 @@
 const myContainer = document.getElementById("container");
-generateNums();
+generateTiles();
 
-console.log("1");
-
-function generateNums(){
+function generateTiles(){
     for(let i = 0; i < 9; i++){
-        myContainer.innerHTML += `<div class="tiles" id="tile-${i+1}" style="background-color: white; border-radius: 10px; height: 30%; width: 31%;"></div>`;
+        myContainer.innerHTML += `
+        <div class="tiles" id="tile-${i+1}" style="
+        background-color: white; 
+        border-radius: 10px; 
+        height: clamp(90px,10vw,10vw); 
+        width: clamp(90px,10vw,10vw);
+        margin: 1%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        ">${i + 1}</div>`;
     } 
+}
+
+const select = () => {
+    const setTiles = document.getElementById("");
+    changeTurn();
+}
+isTurned = true;
+const changeTurn = () => {
+    const imageTurner = document.getElementById("image-turner");
+
+    if(isTurned){
+        imageTurner.src = "images/o.png";
+    } else{
+        imageTurner.src = "images/x.png";
+    }
+    
+    isTurned = !isTurned;
 }
