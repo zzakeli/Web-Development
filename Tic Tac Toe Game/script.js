e92ca66e0a19934812643e92191f0358fdf04738
Jzy <jestalycastillo15@gmail.com>
2025-03-22 23:30:03 +0800

    added selector changer

--- Tic Tac Toe Game/script.js
+++ Tic Tac Toe Game/script.js
@@ -1,10 +1,10 @@
 const myContainer = document.getElementById("container");
 generateTiles();
 
-function generateTiles(){
-    for(let i = 0; i < 9; i++){
+function generateTiles() {
+    for (let i = 0; i < 9; i++) {
         myContainer.innerHTML += `
-        <div class="tiles" id="tile-${i+1}" style="
+        <div class="tiles" id="tile-${i + 1}" style="
         background-color: white; 
         border-radius: 10px; 
         height: clamp(90px,10vw,10vw); 
@@ -14,7 +14,7 @@ function generateTiles(){
         align-items: center;
         justify-content: center;
         ">${i + 1}</div>`;
-    } 
+    }
 }
 
 const select = () => {
@@ -25,11 +25,11 @@ isTurned = true;
 const changeTurn = () => {
     const imageTurner = document.getElementById("image-turner");
 
-    if(isTurned){
+    if (isTurned) {
         imageTurner.src = "images/o.png";
-    } else{
+    } else {
         imageTurner.src = "images/x.png";
     }
-    
+
     isTurned = !isTurned;
 }
